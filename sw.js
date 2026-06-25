2a9a01e955a2ebd70fe50ec3ee5676e3af7146cb
Logic Bomb <joseantoniokt@gmail.com>
2026-06-24 22:44:10 -0400

    app

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'conuco-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+// Instalar y guardar en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar peticiones y servir desde caché si no hay red
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si está en el caché, devuélvelo. Si no, búscalo en la red.
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Limpiar cachés antiguos si actualizas la versión de la app
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
